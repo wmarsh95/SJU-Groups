a29dcc77dcef052ffe6b0446abae102379038f26
wmarsh95 <wmarsh95@gmail.com>
2018-07-26 10:53:55 -0400

    Update function.php

--- function.php
+++ function.php
@@ -11,18 +11,9 @@ Version: 1.0
 add_action('user_register', 'uag_user_register' );
 
 function uag_user_register ( $user_id ) {
-    $curr_user_id = get_current_user_id();
-// the value is 0 if the user isn't logged-in
-    if ( $curr_user_id != 0 ) {
-    // we know now the user is logged-in, so we can use his/her ID to get the user meta
-    $um_value = get_user_meta( $curr_user_id, 'field_5b593b82ce458', true );
-    // now we check if the value is correct
-    if ( ! empty( $um_value ) && $um_value == 'University Couuncil' ) {
-        // if so we can output something
-        echo 'user meta has the value'; {
-                if ( $group = Groups_Group::read_by_name( 'University Council' ) ) {
+        if ($user_id != null) {
+                if ( $group = Groups_Group::read_by_name( 'Premium' ) ) {
                         $result = Groups_User_Group::create( array( "user_id"=>$user_id, "group_id"=>$group->group_id ) );
                 }
         }
 }
-?>
