c3a0dcaf14ed995357801ac09820ff5224684a27
wmarsh95 <wmarsh95@gmail.com>
2018-07-26 13:12:50 -0400

    Update function.php

--- function.php
+++ function.php
@@ -14,8 +14,9 @@ Version: 1.0
 add_action('user_register', 'uag_user_register' );
 
 function uag_user_register ( $user_id ) {
-        if ($user_id != null) {
-                if ( $group = Groups_Group::read_by_name( 'Premium' ) ) {
+        get_user_meta($user_id, $key, $single);
+        if (field_5b593b82ce458 != 'University Council') {
+                if ( $group = Groups_Group::read_by_name( 'University Council' ) ) {
                         $result = Groups_User_Group::create( array( "user_id"=>$user_id, "group_id"=>$group->group_id ) );
                 }
         }
