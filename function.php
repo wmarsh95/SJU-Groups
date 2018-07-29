d0f2e59b10da4b3270f304220a9229d44cbb6cd9
wmarsh95 <wmarsh95@gmail.com>
2018-07-29 18:43:31 -0400

    Update function.php

--- function.php
+++ function.php
@@ -1,23 +1,13 @@
 <?php
-/*
-Plugin name: SJU Groups
-Plugin URI: https://governance.sju.edu/
-Description: A plugin to assign users to groups via ACF metadata, forked from eggemplo (https://github.com/eggemplo/)
-Author: Will Marsh
-Author URI: http://www.sju.edu
-Version: 1.0
-*/
-
 /**
- * Automatically add user to specific group via metadata.
+ * Automatically add user to specific group via Formidable and ACF.
  */
-add_action('user_register', 'uag_user_register' );
-
-function uag_user_register ( $user_id ) {
-        get_user_meta($user_id, $key, $single);
+add_action( 'frmreg_after_create_user', 'do_custom_action_after_registration', 10, 2 );
+function do_custom_action_after_registration( $user_id, $args ) {
+   if ( $args['entry']->form_id == 15 ) {// Replace 15 with your form ID
         if (field_5b593b82ce458 != 'University Council') {
                 if ( $group = Groups_Group::read_by_name( 'University Council' ) ) {
                         $result = Groups_User_Group::create( array( "user_id"=>$user_id, "group_id"=>$group->group_id ) );
-                }
-        }
+   }
 }
+ /?>          
