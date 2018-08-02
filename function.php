1288c4a1c1c158aa4c50022156194ef98b27100c
wmarsh95 <wmarsh95@gmail.com>
2018-08-01 21:50:20 -0400

    Update function.php

--- function.php
+++ function.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin name: SJU Groups
+Plugin name: !SJU Groups
 Plugin URI: https://governance.sju.edu/
 Description: A plugin to assign users to groups via ACF metadata and Foridable Forms.
 Author: Will Marsh
@@ -11,12 +11,11 @@ Version: 1.0
 /**
  * Automatically add user to specific group via Formidable and ACF.
  */
-add_action( 'frmreg_after_create_user', 'do_custom_action_after_registration', 10, 2 );
-function do_custom_action_after_registration( $user_id, $args ) {
-   if ( $args['entry']->form_id == 15 ) {// Replace 15 with your form ID
-        if (field_5b593b82ce458 != 'University Council') {
-                if ( $group = Groups_Group::read_by_name( 'University Council' ) ) {
-                        $result = Groups_User_Group::create( array( "user_id"=>$user_id, "group_id"=>$group->group_id ) );
-   }
-}
- /?>          
+add_action('user_register', 'uag_user_register' );
+
+function uag_user_register ( $user_id ) {
+       echo '<pre>';
+       print_r($_POST);
+       echo '</pre>';
+       exit();
+}        
